ae10d4aca8cfad1321797f3f379a8ddfd06831c2
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2019-11-04 23:09:35 -0500

    added email field

--- database/models/user.js
+++ database/models/user.js
@@ -8,6 +8,11 @@ const userSchema = new Schema({
         unique: true,
         required: true,
     },
+    email: {
+      type: String,
+      unique: true,
+        required: true,
+    },
     name: {
         type: String,
         required: true,
