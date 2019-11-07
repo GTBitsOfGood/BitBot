b567029655fca45437d8046da7eacabde8b3319b
jlefelhocz <jlefelhocz@yahoo.com>
2019-11-06 21:47:18 -0500

    testing part 2

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,11 @@
 const { App } = require('@slack/bolt');
 require('./config');
 
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
 app.message('boo', ({ message, say }) => {
-  say(`Hey there <@${message.user}>!`);
+  say(`Hey <@${message.user}>!`);
 });
