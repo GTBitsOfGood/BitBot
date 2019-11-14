a7c5313094c8b7a3d4761670bc20c8a8e87045ba
Sukhmai Kapur <39801345+Sukhmai@users.noreply.github.com>
2019-11-13 21:52:31 -0500

    added commands endpoint

--- app/app.js
+++ app/app.js
@@ -5,7 +5,11 @@ const { App } = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  endpoints: {
+    events: '/slack/events',
+    commands: '/slack/commands'
+  }
 });
 
 app.message('hello', ({ message, say }) => {
